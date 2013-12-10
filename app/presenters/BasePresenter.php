aea23027d1dad4468d65895b9603d0a93ad55cf3
frosty22 <ledvinka.vit@gmail.com>
2013-12-10 09:24:54 +0100

    Implements Composer Components package

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -11,6 +11,21 @@ namespace App;
 abstract class BasePresenter extends \Ale\Application\UI\Presenter {
 
 
+	/**
+	 * @persistent
+	 * @var string
+	 */
+	public $backlink;
 
+
+	/**
+	 * @param string $name
+	 * @param \ComposerComponents\Control\Control $control
+	 * @return \ComposerComponents\Control\Control
+	 */
+	protected function createComponentComponents($name, \ComposerComponents\Control\Control $control)
+	{
+		return $control;
+	}
 
 }
