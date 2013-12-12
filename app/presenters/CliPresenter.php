1d7c8077db3375c9af6afa059b286dcde3223f71
frosty22 <ledvinka.vit@gmail.com>
2013-12-12 15:28:18 +0100

    some deployment fixes

---
+++ app/presenters/CliPresenter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\KdybyModule;
+
+/**
+ *
+ * @copyright Copyright (c) 2013 Ledvinka Vít
+ * @author Ledvinka Vít, frosty22 <ledvinka.vit@gmail.com>
+ *
+ */
+class CliPresenter extends \KdybyModule\CliPresenter {
+}
